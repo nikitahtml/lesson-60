799eb742af5635797c37dacfe3a909b5e4853046
bibikov nikita <bibikovnikita777@gmail.com>
2024-06-17 16:20:14 +0600

    Create MessageList component to display messages

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,9 +1,29 @@
+import React from 'react';
+import { ListGroup } from 'react-bootstrap';
 
-const MessageList = () => {
+interface Message {
+    _id: string;
+    message: string;
+    author: string;
+    datetime: string;
+}
+
+interface MessageListProps {
+    messages: Message[];
+}
+
+const MessageList: React.FC<MessageListProps> = ({ messages }) => {
     return (
-        <div>
-
-        </div>
+        <ListGroup>
+            {messages.map((msg) => (
+                <ListGroup.Item key={msg._id}>
+                    <strong>{msg.author}</strong>: {msg.message}
+                    <div className="text-muted" style={{ fontSize: '0.8em' }}>
+                        {new Date(msg.datetime).toLocaleString()}
+                    </div>
+                </ListGroup.Item>
+            ))}
+        </ListGroup>
     );
 };
 
